3ee4420d6ac2d8696fecda580984b5795a8fef0f
User-PC <2356532@mail.ru>
2026-02-20 12:29:35 +0500

    ДЗ по теме: "Unit-тестирование и mock-объекты - исправленное"

--- org/skypro/skyshop/service/SearchService.java
+++ org/skypro/skyshop/service/SearchService.java
@@ -16,23 +16,6 @@ public class SearchService {
         this.storageService = storageService;
     }
 
-//    private boolean matchesQuery(String text, String query) {
-//        if (text == null || query == null) {
-//            return false;
-//        }
-//        String lowerText = text.toLowerCase();
-//        String lowerQuery = query.toLowerCase();
-//        return lowerText.contains(lowerQuery);
-//    }
-
-//    public Collection<SearchResult> search(@RequestParam String pattern){
-//        Collection<Searchable> allSearchable = storageService.articlesAndProducts();
-//        return allSearchable.stream()
-//                .filter(searchable -> searchable.getSearchTerm().contains(pattern))
-//                .map(SearchResult::fromSearchable)
-//                .collect(Collectors.toList());
-//    }
-
     public Collection<SearchResult> search(@RequestParam String pattern) {
         System.out.println("→ Поиск запущен. Запрос: \"" + pattern + "\"");
 
